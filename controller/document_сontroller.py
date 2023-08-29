cdd257d99f394d252611c02ea37e68ccfa6bffaf
Jacobzzzzzzz <yakov.komar@mail.ru>
2023-08-29 12:00:51 +0300

    :sparkles: Db-related HTMLResponse
    new html response, bug fixes and static files update

--- controller/document_сontroller.py
+++ controller/document_сontroller.py
@@ -1,6 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from starlette.requests import Request
+from starlette.responses import HTMLResponse
+
 from database import get_db
 from service.document_service import DocumentService
 from dto.request import document_request
@@ -8,7 +10,6 @@ from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 
 router = InferringRouter(tags=["document"])
-
 
 @cbv(router)
 class DocumentController:
@@ -23,7 +24,7 @@ class DocumentController:
     async def delete_all(self):
         return await self.document_service.delete_all(self.session)
 
-    @router.get('/{guid}')
+    @router.get('/{guid}', response_class=HTMLResponse)
     async def get(self, guid: str):
         return await self.document_service.get_document(self.session, guid)
 
--- service/document_service.py
+++ service/document_service.py
@@ -1,4 +1,9 @@
+from html.parser import HTMLParser
+
+from fastapi import HTTPException
 from starlette.requests import Request
+from starlette.responses import HTMLResponse
+
 from model.document import Document
 from sqlalchemy.orm import Session
 from fastapi.templating import Jinja2Templates
@@ -8,12 +13,13 @@ from dto.response.simple_message_response import SimpleMessageResponse
 from utils.env_variables_reader import EnvVariablesReader
 import uuid
 from bs4 import BeautifulSoup
+
 templates = Jinja2Templates(directory="templates")
 
 
 class DocumentService:
 
-    async def create_document(self, db: Session,  request: Request, data: document_request.DocumentRequest):
+    async def create_document(self, db: Session, request: Request, data: document_request.DocumentRequest):
         template = templates.TemplateResponse("document.html", {"request": request, "content": data.content})
         soup = BeautifulSoup(template.body)
         document = Document(content=str(soup))
@@ -24,14 +30,26 @@ class DocumentService:
         return SimpleMessageResponse(f"https://{domain}/{document.guid}")
 
     async def get_document(self, db: Session, guid: str):
-        return db.query(Document).filter(Document.guid == uuid.UUID(guid)).first()
+        document = db.query(Document).filter(Document.guid == uuid.UUID(guid)).first()
+        res = HTMLResponse(document.content).body
+        formatted_res = res.decode('utf-8')
+        print(formatted_res)
+        return HTMLResponse(content=formatted_res)
 
     async def delete(self, db: Session, guid: str):
-        document = db.query(Document).filter(Document.guid == uuid.UUID(guid)).delete()
-        db.commit()
-        return SimpleMessageResponse("Successfully deleted")
+        document = db.query(Document).filter(Document.guid == uuid.UUID(guid)).first()
+        if document is not None:
+            db.query(Document).filter(Document.guid == uuid.UUID(guid)).delete()
+            db.commit()
+            return SimpleMessageResponse("Successfully deleted")
+        else:
+            raise HTTPException(status_code=404, detail="document not found")
 
     async def delete_all(self, db: Session):
-        db.query(Document).delete()
-        db.commit()
-        return SimpleMessageResponse("Successfully deleted")
+        document = db.query(Document).first()
+        if document is not None:
+            db.query(Document).delete()
+            db.commit()
+            return SimpleMessageResponse("Successfully deleted")
+        else:
+            raise HTTPException(status_code=404, detail="documents not found")
